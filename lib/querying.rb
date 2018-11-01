fce73efbb1687a6d508b6b4637f7ae10b4f76d6b
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 21:48:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-
+ "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species"
 end
 
 def select_name_and_series_subgenres_of_authors
