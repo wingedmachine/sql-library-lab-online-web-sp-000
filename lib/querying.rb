b2a8f03762aeb3b8d71e0f769818f854343453ce
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 21:46:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,8 @@ end
 def select_name_and_motto_of_char_with_longest_motto
  "SELECT name, motto
   FROM characters
-  WHERE id = (SELECT id, LENGTH(motto) FROM characters ORDER BY LENGTH(motto) LIMIT 1)"
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1"
 end
 
 
