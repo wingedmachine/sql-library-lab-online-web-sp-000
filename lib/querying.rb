551499d3897eb448447fad0ab37c9813a9c0ae88
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 21:59:27 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,9 +31,7 @@ end
 
 def select_series_title_with_most_human_characters
  "SELECT *
-  FROM series
-  GROUP BY species
-  ORDER BY COUNT(species) DESC"
+  FROM series"
 end
 
 def select_character_names_and_number_of_books_they_are_in
