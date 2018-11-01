9c4568f58a640c0eed2b47a6df697c0d403dcbe9
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 21:41:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year 
+  "SELECT books.title, year
    FROM books
      JOIN series ON series_id = series.id
    WHERE series.id = 1
