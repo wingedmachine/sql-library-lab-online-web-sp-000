c426aa57d0cc837fc3db48579a3511a6912d164d
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-01 21:52:26 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
- "SELECT authors.name, subgenres.id
+ "SELECT authors.name, subgenres.name
   FROM authors
     JOIN series ON author_id = authors.id
     join subgenres ON subgenre_id = subgenres.id"
